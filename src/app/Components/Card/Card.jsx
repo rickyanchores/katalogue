529401844e368b720060c17f059d4d0f03cbfaaa
RickyAnchores <ricky23anchores@gmail.com>
2024-10-31 12:52:45 +0100

    update

--- src/app/Components/Card/Card.jsx
+++ src/app/Components/Card/Card.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Card = ({title,image}) => {
   return (
     <div className="Card">
-        <img src={image} alt="image" />
+        <img className='w-[500px]' src={image} alt="image" />
         <div className="description">
-            <p>{title}</p>
+            <p className='text-2xl font-bold'>{title}</p>
         </div>
     </div>
   )
--- src/app/Pages/About/About.jsx
+++ src/app/Pages/About/About.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
 const About = () => {
+
   return (
-    <div className='bg-gray-300 text-black h-screen grid items-center'>
+    <div className='bg-gray-300 text-black h-screen grid items-center grid-cols-1 md:grid-cols-2'>
         <h1 className='text-[5em] md:text-[200px] font-extrabold'>About</h1>
+        <img className='w-[600px]' src="https://www.seikoboutique.eu/it/10322-superZoom/orologio-automatico-prospex-diver-300m-quadrante-blu.jpg" alt="imageAbout" />
     </div>
   )
 }
 
 export default About;
+
+
