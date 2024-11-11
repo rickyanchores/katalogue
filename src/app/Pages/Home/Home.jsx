18297579cc876f8ccaaaf61fcbf50aa9f627cbc2
RickyAnchores <ricky23anchores@gmail.com>
2024-11-11 10:01:22 +0100

    update HOME

--- src/app/Pages/Home/Home.jsx
+++ src/app/Pages/Home/Home.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='Home bg-gray-300 hover:bg-orange-600 text-black h-screen grid items-center' id='/'>
+    <div className='Home bg-gray-300 hover:bg-orange-600 text-black h-screen grid items-center justify-center' id='/'>
         <h1 className='title font-semibold text-[5em] md:text-[300px]'>Katalogue</h1>
-        <h2>Modern. Slick. Catalogue.</h2>
+        <h2 className='grid justify-end text-[2em] mr-4'>Modern. Slick. Catalogue.</h2>
     </div>
   )
 }
