326fd4395e484dbfed46945bece4045e276188df
RickyAnchores <ricky23anchores@gmail.com>
2025-03-19 09:21:54 +0100

    update

--- src/app/Components/Nav/Nav.jsx
+++ src/app/Components/Nav/Nav.jsx
@@ -22,7 +22,7 @@ const Nav = () => {
         }
     ]
   return (
-    <nav className='Nav fixed w-full bg-black text-white grid items-center grid-cols-2 p-4'>
+    <nav className='Nav fixed w-full text-black grid items-center grid-cols-2 p-4'>
         <h1 className='text-2xl font-extrabold'>KAT</h1>
         <ul className="navLinks flex font-bold gap-4 ">
            {navList.map((link,index) => (
--- src/app/Pages/GridGallery/GridGallery.jsx
+++ src/app/Pages/GridGallery/GridGallery.jsx
@@ -4,10 +4,10 @@ const GridGallery = () => {
   return (
     <div className='GridGallery h-screen grid items-center'>
         <div className="grid-container h-screen p-4 grid grid-cols-5 grid-rows-5 gap-4">
-            <div className="bg-slate-600 hover:bg-orange-600 row-span-2 col-start-3 row-start-1">1</div>
-            <div className="bg-slate-600 hover:bg-orange-600 col-span-2 row-span-2 col-start-4 row-start-1">2</div>
-            <div className=" bg-slate-600 hover:bg-orange-600 col-span-2 row-span-5 col-start-1 row-start-1">3</div>
-            <div className="bg-slate-600 hover:bg-orange-600 col-span-3 row-span-3 col-start-3 row-start-3">4</div>
+            <div className="bg-gray-300 hover:bg-orange-600 row-span-2 col-start-3 row-start-1">1</div>
+            <div className="bg-gray-300 hover:bg-orange-600 col-span-2 row-span-2 col-start-4 row-start-1">2</div>
+            <div className=" bg-gray-300 hover:bg-orange-600 col-span-2 row-span-5 col-start-1 row-start-1">3</div>
+            <div className="bg-gray-300 hover:bg-orange-600 col-span-3 row-span-3 col-start-3 row-start-3">4</div>
         </div>
     </div>
   )
